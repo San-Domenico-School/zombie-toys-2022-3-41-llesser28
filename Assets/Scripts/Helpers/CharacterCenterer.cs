fe91b737f3b6b68350e666a39af5a623421e25f3
llesser28 <llesser28@sandomenico.org>
2025-01-13 14:59:59 -0800

    Characters model complete

---
+++ Assets/Scripts/Helpers/CharacterCenterer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCenterer : MonoBehaviour
+{
+    // Awake is called before the first frame update
+    void Start()
+    {
+        transform.localPosition = Vector3.zero;
+
+    }
+
+   
+}
